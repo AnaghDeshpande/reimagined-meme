189b818fc9869a24505cf92c2b71a1fa84b8ed8f
AnaghDeshpande <anaghdeshpande257@gmail.com>
2025-09-03 13:48:05 +0530

    done some work

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data analyst",
+        "location": "Bengaluru, India",
+        "Salary": "Rs. 10,00,000"
+    },
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Delhi, India",
+        # "Salary": "Rs. 15,00,000"
+    },
+    {
+        "id": 3,
+        "title": "Backend Engineer",
+        "location": "Bengaluru, India",
+        "Salary": "Rs. 10,00,000"
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html", jobs=JOBS)
+
+@app.route("/api /jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
